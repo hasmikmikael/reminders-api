f838154c124c6016d3cd38d5756a05ce4ef698a1
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-13 03:31:50 +0400

    Creating a Basic Route.

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 import express from 'express';
 const app = express();
-app.listen(3000, () => console.log('Server started'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(4000, () => console.log('Server started'));
